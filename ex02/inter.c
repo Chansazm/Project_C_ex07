b7e447bb187821e93875f77cda1a636d73f37352
chansa chishimba <chishimb_c@qwasar.io>
2020-07-02 14:16:14 +0000

    Added intersection and Union of strings

--- ex02/inter.c
+++ ex02/inter.c
@@ -1,31 +1,85 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-char* inter(char* param_1, char* param_2){
+int my_str_index(char *s, char c) {
+    int index = 0;
 
-    int i = 0, j = 0; //char * Result;
-    //int n = strlen(param_1 -1);
-    //int m = strlen(param_2 -1);
-    char * result = 0;
-    while (param_1[i] != '\0'){
-        if (param_1[i] < param_2[j])
-            i++;
-        else if(param_1[i] > param_1[j])
-            j++;
-     
-        else
-            printf("%c",param_1[i]);
-    
-    
+    while (s[index] != '\0') {
+        if (s[index] == c) {
+            return index;
+        }
+        index += 1;
     }
-    return 0;
+    return -1;
+}
+
+int my_strlen(char *str) {
+    int index = 0;
+
+    while (str[index]) {
+        index += 1;
     }
+    return index;
+}
+
+void my_push_character(char *dest, char c) {
+    int index = 0;
+
+    while (dest[index] != '\0') {
+        index += 1;
+    }
+    dest[index] = c;
+    dest[index + 1] = '\0'; 
+}
+
+char* inter(char* param_1, char* param_2)
+{
+    int index = 0;
+    char *result = malloc(sizeof(*result) * (my_strlen(param_1) + 1));
+
+    result[0] = '\0';
+    while (param_1[index] != '\0') {
+        int found_index = my_str_index(param_1, param_1[index]);
+
+        if (found_index == index) {
+            if (my_str_index(param_2, param_1[index]) != -1) {
+                my_push_character(result, param_1[index]);
+            }
+        }
+        index += 1;
+    }
+    return result;
+}
+/*
 
 
-int main(){
-    char param_1[] = {1,2,4,7};
-    char param_2[] = {3,2,5,7};
-    inter(param_1, param_2);
+char* mystrcpy(char *dst, char *src)
+{
+    int index = 0;
 
+    while (src[index] != '\0') {
+        dst[index] = src[index];
+    }
+    return dst;
+}
+*/
+//
+// padinto
+//
+// s1 - padinton
+// s2 - paqefwtdjetyiytjneytjoeyjnejeyj
+// result << painto
+
+// padinto
+
+int main(int ac, char **av) {
+//    char* param_1 = "padinton";
+//    char* param_2 = "paqefwtjetyiytjneytjdoeyjnejeyj";
+
+    // ???? <-- 0
+//    inter(param_1, param_2);
+    char *s = inter(av[1], av[2]);
+    printf("RESULT IS %s\n", s);
+    free(s);
     return 0;
 }
--- ex03/rcapitalize.c
+++ ex03/rcapitalize.c
@@ -1,27 +1,107 @@
 #include <string.h>
-//#include <ctype.h>
 #include <stdio.h>
-
-char* rcapitalize(char* param_1){
-int length = strlen(param_1);
-//printf("%d\n",length);
+#include <stdbool.h>
 
 
-for( int i = 0; i < length; i++){
-    if (param_1[i] == ' ' || param_1[i] == '\0') 
+void my_swap(char *a, char *b) {
+    char tmp = *a;
+
+    *a = *b;
+    *b = tmp;
+}
+
+char *reverse_string(char* param_1) {
+    int begin_index = 0;
+    int end_index = strlen(param_1) - 1;
+
+    while (begin_index < end_index)
     {
-      param_1[i-1] = param_1[i] - 'a' + 'A'; 
-      
+        my_swap(&param_1[begin_index], &param_1[end_index]);
+
+        begin_index += 1;
+        end_index   -= 1;
     }
+    return param_1;
 }
 
-return param_1;
+bool my_isalpha(char c) {
+    if ('a' <= c && c <= 'z') {
+        return true;
+    }
+    if ('A' <= c && c <= 'Z') {
+        return true;
+    }
+    return false;
 }
 
-int main(){
-    char* param_1 = "a FiRSt LiTTlE TESt";
-    rcapitalize(param_1);
+
+bool my_isalpha_capitalize(char c) {
+    if ('A' <= c && c <= 'Z') {
+        return true;
+    }
+    return false;
+}
+
+char *my_downcase(char *str) {
+    int index = 0;
+
+    while (str[index] != '\0') {
+        if (my_isalpha_capitalize(str[index]) == true) {
+            str[index] += 32;
+        }
+        index += 1;
+    }
+    return str;
+}
+
+/*
+a FiRSt LiTTlE TESt
+
+A firsT littlE tesT
+-> downcase and upcase the last letter
+
+1# downcase on the string
+a first little test
+
+2# upcase the last letter
+2a# reverse string
+      tset elttil tsrif a
+2b#
+while until end of string
+    while space
+    if letter
+       upcase letter of word
+    while word
+*/
+
+char* capitalize(char* param_1){
+    int index = 0;
+
+    while (param_1[index] != '\0') {
+        // GO THROUGHT SPACE
+       while (param_1[index] != '\0' && param_1[index] == ' ') {
+           index += 1;
+       }
+       if (my_isalpha(param_1[index]) == true) {
+           param_1[index] -= 32;
+       }
+       while (param_1[index] != '\0' && param_1[index] != ' ') {
+           index += 1;
+       }
+    }
+    return param_1;
+}
+
+
+int main(int ac, char **av) {
+    char* param_1 = strdup(av[1]);
+//    char* param_1 = strdup("a FiRSt LiTTlE TESt");
+
+    my_downcase(param_1);
+    reverse_string(param_1);
+    capitalize(param_1);
+    reverse_string(param_1);
+
+    printf("%s\n", param_1);
     return 0;
 }
-//Input: "a FiRSt LiTTlE TESt"
-//Return Value: "A firsT littlE tesT"
