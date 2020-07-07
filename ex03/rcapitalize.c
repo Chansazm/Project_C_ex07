a673fcff70d7f2be1a6f982f393357a1c836be66
chansa chishimba <chishimb_c@qwasar.io>
2020-07-07 19:56:21 +0000

    Added capitalization

--- ex03/rcapitalize.c
+++ ex03/rcapitalize.c
@@ -1,108 +1,46 @@
-#include <string.h>
-#include <stdio.h>
-#include <stdbool.h>
-
-
-void my_swap(char *a, char *b) {
-    char tmp = *a;
-
-    *a = *b;
-    *b = tmp;
-}
-
-char *reverse_string(char* param_1) {
-    int begin_index = 0;
-    int end_index = strlen(param_1) - 1;
-
-    while (begin_index < end_index)
+char *my_strrev(char *str, int len)
+{
+    for (int i = 0; i < len / 2; i++)
     {
-        my_swap(&param_1[begin_index], &param_1[end_index]);
-
-        begin_index += 1;
-        end_index   -= 1;
-    }
-    return param_1;
-}
-
-bool my_isalpha(char c) {
-    if ('a' <= c && c <= 'z') {
-        return true;
-    }
-    if ('A' <= c && c <= 'Z') {
-        return true;
-    }
-    return false;
-}
-
-
-bool my_isalpha_capitalize(char c) {
-    if ('A' <= c && c <= 'Z') {
-        return true;
-    }
-    return false;
-}
-
-char *my_downcase(char *str) {
-    int index = 0;
-
-    while (str[index] != '\0') {
-        if (my_isalpha_capitalize(str[index]) == true) {
-            str[index] += 32;
-        }
-        index += 1;
+        char temp = str[i];
+        str[i] = str[len - 1 - i];
+        str[len - 1 -i] = temp;
     }
     return str;
 }
 
-/*
-a FiRSt LiTTlE TESt
+char *rcapitalize(char *str)
+{
+    int length = strlen(str);
+    my_strrev(str, length);
+    
+        for(int i = 0; str[i] != '\0'; i++)
+        {
+            if (i == 0)
+            {
+                if ((str[i]>= 'a' && str[i] <= 'z'))
+                str[i] = str[i] - 32;
+                    continue;
+            }
+            if(str[i] == ' ')
+            {
+                while(str[i] == ' ')
+                {
+                    i++;
+                }
+                if(str[i] >= 'a' && str[i] <= 'z')
+                {
+                    str[i] = str[i] - 32;
+                    continue;
+                }
+            }
+            else 
+            {
+                if(str[i] >= 'A' && str[i] <= 'Z')
 
-A firsT littlE tesT
--> downcase and upcase the last letter
-
-1# downcase on the string
-a first little test
-
-2# upcase the last letter
-2a# reverse string
-      tset elttil tsrif a
-2b#
-while until end of string
-    while space
-    if letter
-       upcase letter of word
-    while word
-*/
-
-char* capitalize(char* param_1){
-    int index = 0;
-
-    while (param_1[index] != '\0') {
-        // GO THROUGHT SPACE
-       while (param_1[index] != '\0' && param_1[index] == ' ') {
-           index += 1;
-       }
-       if (my_isalpha(param_1[index]) == true) {
-           param_1[index] -= 32;
-       }
-       while (param_1[index] != '\0' && param_1[index] != ' ') {
-           index += 1;
-       }
-    }
-    return param_1;
+                    str[i] = str[i] + 32;
+            }
+        }
+        my_strrev(str,length);
+        return str;
 }
-
-/*
-int main(int ac, char **av) {
-    char* param_1 = strdup(av[1]);
-//    char* param_1 = strdup("a FiRSt LiTTlE TESt");
-
-    my_downcase(param_1);
-    reverse_string(param_1);
-    capitalize(param_1);
-    reverse_string(param_1);
-
-    printf("%s\n", param_1);
-    return 0;
-}
-*/
