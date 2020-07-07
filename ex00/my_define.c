ec475eb64b492fc19270281912e8920512fd19ac
chansa chishimba <chishimb_c@qwasar.io>
2020-07-07 19:57:22 +0000

    Added my define

--- ex00/my_define.c
+++ ex00/my_define.c
@@ -1,34 +1,41 @@
-#define FALSE 0
-#define TRUE 1
+#include <unistd.h>
+#define EVEN(nbr) ((nbr % 2 != 0)? TRUE : FALSE)
 #define SUCCESS 0
-#define EVEN(a) ((a % 2 == 0) ? TRUE : FALSE)
 #define EVEN_MSG "I have an even number of arguments." 
 #define ODD_MSG "I have an odd number of arguments."
 
+typedef enum s_bool
+{
+    TRUE,
+    FALSE
+
+} t_bool;
+
+void my_putchar(char c)
+{
+    write(1, &c, 1);
+}
 
 void	my_putstr(char *str)
 {
-    int index = 0;
+    int index;
+
+    index = 0;
 	while (str[index] != '\0') {
-		write(1, &str[index], 1);
+		my_putchar(str[index]);
 		index++;
 	}
 }
 
-_Bool	my_is_even(int nbr){
-	return (EVEN(nbr) ? TRUE : FALSE);
+t_bool my_is_even(int nbr)
+{
+    return ((EVEN(nbr)) ? TRUE : FALSE);
 }
 
-int		my_main(int argc)
+void my_define(int argc)
 {
-	if (my_is_even(argc - 1) == TRUE)
-		my_putstr(EVEN_MSG);
-	else
-		my_putstr(ODD_MSG);
-	return (SUCCESS);
-}
-
-char* my_define(int param_1)
-{
-    return ((my_is_even(param_1)) ? EVEN_MSG : ODD_MSG);
+    if (my_is_even(argc) == TRUE){
+    my_putstr(EVEN_MSG);
+    my_putchar('\n');
+    }
 }
